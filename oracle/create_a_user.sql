ac3a373945a6c3c38a2bc325e3c40ed7473a2e6b
Soda Smile <soda@sodasmile.com>
2014-12-09 08:53:01 -0800

    removing

--- oracle/create_a_user.sql
+++ oracle/create_a_user.sql
@@ -1,1 +1,33 @@
-../../asm/create_a_user.sql
+set echo on
+
+-- SET SERVEROUTPUT ON;
+
+accept vstring prompt "Please enter your name: ";
+
+declare
+   v_line varchar2(40);
+   schema varchar2(40);
+begin
+   schema := '&vstring';
+   v_line := 'Hello '|| schema;
+   dbms_output.put_line(v_line);
+   begin
+      execute immediate 'DROP USER ' || schema || ' CASCADE';
+   exception 
+     when others 
+     then
+        dbms_output.put_line('User: ' || schema || ' not exists');	
+   end;
+--   spool 'create_' || schema || '.log';
+   execute immediate 'CREATE USER ' || schema || ' IDENTIFIED BY TEST DEFAULT TABLESPACE DATA TEMPORARY TABLESPACE TEMP PROFILE DEFAULT ACCOUNT UNLOCK';
+   execute immediate 'GRANT RESOURCE TO ' || schema;
+   execute immediate 'GRANT DBA TO ' || schema;
+   execute immediate 'GRANT CONNECT TO ' || schema;
+   execute immediate 'ALTER USER ' || schema || ' DEFAULT ROLE ALL';
+   execute immediate 'GRANT GLOBAL QUERY REWRITE TO ' || schema;
+   execute immediate 'GRANT UNLIMITED TABLESPACE TO ' || schema;
+end;
+/
+
+exit;
+
