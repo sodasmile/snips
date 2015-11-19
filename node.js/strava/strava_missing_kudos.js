70cf46497858f86fe11319087bf65361d1c922d1
Pi Raspi <pi@raspi.weez.hf1>
2015-11-19 22:26:00 +0100

    Moving strava to strava package.

---
+++ node.js/strava/strava_missing_kudos.js
@@ -0,0 +1,58 @@
+// List chosen following athletes with activities without kudos
+
+var https = require('https');
+var fs = require('fs');
+//var xmpp = require(__dirname + '/xmpp')
+
+var settings = JSON.parse(fs.readFileSync(__dirname + '/settings.json'));
+
+var MILLIS_IN_SECOND = 1000;
+
+var before = null;
+
+var options = {
+    hostname: 'www.strava.com',
+    port: 443,
+    path: '/api/v3/activities/following' + (before!=null?'?before=' + before:''),
+    method: 'GET',
+    headers: {
+     "Authorization": "Bearer " + settings.strava_api_key
+    }  
+};
+
+//xmpp.sendMessage(settings.notification_receiver, 'hei')
+
+var req = https.request(options, function(res) {
+
+  var outp = '';
+  var oldest = Number.MAX_VALUE;
+
+  res.on('data', function(d) {
+    outp += d;
+  });
+
+  res.on('end', function() {
+    pars = JSON.parse(outp);
+    for (var i = 0; i < pars.length; i++) {
+        var activity = pars[i];
+        var athlete = activity.athlete;
+        var timestamp = Date.parse(activity.start_date);
+        if (settings.athleteids.indexOf(athlete.id) != -1 && !activity.has_kudoed) {
+           var started = new Date(timestamp);
+           var message = "Missing kudos on " + athlete.firstname + "s activity: https://www.strava.com/activities/" + activity.id + " started " + started;
+           console.log(message);
+           //xmpp.sendMessage(settings.notification_receiver, message)
+        }
+        oldest = Math.min(oldest, timestamp);       
+    }
+
+    console.log("Oldest timestamp: " + oldest/MILLIS_IN_SECOND);
+  });
+});
+
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
+
