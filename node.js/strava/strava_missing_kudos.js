dbfd7510d3c42504803f0765293ae73d65f4b352
Pi Raspi <pi@raspi.weez.hf1>
2016-07-05 20:48:17 +0200

    Only notifying each new activity one time, not every 5 minutes.

--- node.js/strava/strava_missing_kudos.js
+++ node.js/strava/strava_missing_kudos.js
@@ -4,7 +4,13 @@ var https = require('https');
 var fs = require('fs');
 var xmpp = require(__dirname + '/xmpp')
 
+var notificationDBFile = "notification.db.json"
+
 var settings = JSON.parse(fs.readFileSync(__dirname + '/settings.json'));
+var notificationDB = JSON.parse(fs.readFileSync(__dirname + '/' + notificationDBFile));
+if (!notificationDB.notifiedActivities) {
+    notificationDB.notifiedActivities = new Array()
+}
 
 var MILLIS_IN_SECOND = 1000;
 
@@ -31,6 +37,7 @@ var req = https.request(options, function(res) {
 
   res.on('end', function() {
     pars = JSON.parse(outp);
+    var newActivity = false
     for (var i = 0; i < pars.length; i++) {
         var activity = pars[i];
         var athlete = activity.athlete;
@@ -38,7 +45,10 @@ var req = https.request(options, function(res) {
         // console.log("Activity: " + athlete.firstname + " " + athlete.lastname + " Duration: " + activity.moving_time + "s, " + (activity.moving_time / 60) + "min")
         if (settings.athleteids.indexOf(athlete.id) != -1 
               && !activity.has_kudoed 
-              && (activity.moving_time / 60 ) >= settings.min_moving_time) {
+              && (activity.moving_time / 60 ) >= settings.min_moving_time
+              && notificationDB.notifiedActivities.indexOf(activity.id) == -1) {
+           newActivity = true
+           notificationDB.notifiedActivities.push(activity.id)
            var started = new Date(timestamp);
            var message = "Missing kudos on " + athlete.firstname + "s activity: https://www.strava.com/activities/" + activity.id + " started " + started;
            console.log(message);
@@ -48,6 +58,15 @@ var req = https.request(options, function(res) {
     }
 
     console.log("Oldest timestamp: " + oldest/MILLIS_IN_SECOND);
+   
+    if (newActivity) { 
+        fs.writeFile(notificationDBFile, JSON.stringify(notificationDB), function(err) {
+            if (err) {
+                console.log("Error: " + err)
+                return
+            }
+        })
+    } 
   });
 });
 
